bdae78c8b02f16866d28f27ae5b9e363b2d87ce1
MarZuleta <martinzuleta@hotmail.com>
2021-10-11 14:13:59 -0300

    Redirect started

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import {Container} from 'reactstrap';
 import {getRoutes} from '@utils';
 import Home from '@pages/Home';
+import {useSelector} from 'react-redux';
 import Header from './header';
 import Footer from './footer';
 
 const mainRoutes = getRoutes('mainRoutes');
 
 const Router = () => {
-    const inAuthenticate = false;
-    if (!inAuthenticate) {
+    const sessionState = useSelector(state => state.session);
+    const {isAuthenticate} = sessionState;
+    if (!isAuthenticate) {
         return (
             <>
                 <Header/>
@@ -18,6 +20,13 @@ const Router = () => {
                     <Switch>
                         <Route exact path={mainRoutes.home} component={Home}/>
                         <Route exact path={mainRoutes.testimonial} component={Home}/>
+                        <Route exact path="/editProfile">
+                            {/* Aca reemplazar path con el que esta definido en mainRoutes */}
+                            <Redirect to={mainRoutes.home}/>
+                        </Route>
+                        <Route exact path="/backOffice">
+                            <Redirect to={mainRoutes.home}/>
+                        </Route>
                     </Switch>
                 </Container>
                 <Footer/>
