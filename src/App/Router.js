e2cbc9eff79af7d1288a933d76086c6e89d3aa3b
MarZuleta <martinzuleta@hotmail.com>
2021-10-12 11:05:10 -0300

    Verificacion de rutas segun token agregado

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import {Container} from 'reactstrap';
 import {getRoutes} from '@utils';
 import Home from '@pages/Home';
-import {useSelector} from 'react-redux';
+import jwt from 'jwt-decode';
 import Header from './header';
 import Footer from './footer';
 
 const mainRoutes = getRoutes('mainRoutes');
 
 const Router = () => {
-    const sessionState = useSelector(state => state.session);
-    const {isAuthenticate} = sessionState;
-    if (!isAuthenticate) {
+    const token = localStorage.getItem('token_agent');
+    const roleId = token ? jwt(token).roleId : null;
+    if (!token) {
+        // Las rutas publicas
         return (
             <>
                 <Header/>
@@ -20,25 +21,33 @@ const Router = () => {
                     <Switch>
                         <Route exact path={mainRoutes.home} component={Home}/>
                         <Route exact path={mainRoutes.testimonial} component={Home}/>
-                        <Route exact path="/editProfile">
-                            {/* Aca reemplazar path con el que esta definido en mainRoutes */}
-                            <Redirect to={mainRoutes.home}/>
-                        </Route>
-                        <Route exact path="/backOffice">
-                            <Redirect to={mainRoutes.home}/>
-                        </Route>
                     </Switch>
                 </Container>
                 <Footer/>
             </>
         );
     }
-
+    if (roleId === 1) {
+        // Rutas admin
+        return (
+            <>
+                <Switch>
+                    <Route exact path={mainRoutes.home} key="activePerson" component={Home}/>
+                </Switch>
+            </>
+        );
+    }
     return (
+        // Rutas para usuario privado
         <>
-            <Switch>
-                <Route exact path={mainRoutes.home} key="activePerson" component={Home}/>
-            </Switch>
+            <Header/>
+            <Container className="background">
+                <Switch>
+                    <Route exact path={mainRoutes.home} component={Home}/>
+                    <Route exact path={mainRoutes.testimonial} component={Home}/>
+                </Switch>
+            </Container>
+            <Footer/>
         </>
     );
 };
